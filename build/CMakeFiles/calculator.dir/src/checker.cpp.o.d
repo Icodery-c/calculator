2bc588e817cf4aa7c9d78f32a021c466e0494257
Icodery-c <islamovilnur@icloud.com>
2026-03-13 22:22:14 +0300

    Delete build directory from repository

--- build/CMakeFiles/calculator.dir/src/checker.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/calculator.dir/src/checker.cpp.o: \
- /home/ilnur-islamov/Calculator/calculator/src/checker.cpp \
- /usr/include/stdc-predef.h \
- /home/ilnur-islamov/Calculator/calculator/include/checker.h \
- /home/ilnur-islamov/Calculator/calculator/include/data.h
--- build/CMakeFiles/calculator.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/calculator.dir/src/main.cpp.o: \
- /home/ilnur-islamov/Calculator/calculator/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/ilnur-islamov/Calculator/calculator/include/runner.h
